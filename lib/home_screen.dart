aa8181ea38d51a1925cfa419af83f2c244d2dec4
Vedant Yogesh <vedantyogesh@gmail.com>
2019-08-30 00:22:24 +0530

    UI and name changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,9 +9,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Widget buildRaisedButton(String val, String value) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 25.0),
+    return Container(
+      height: 70.0,
+      margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 2.0),
+      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
       child: RaisedButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           color: Colors.teal,
           textColor: Colors.white,
           splashColor: Colors.blueGrey,
@@ -39,14 +43,14 @@ class _HomeScreenState extends State<HomeScreen> {
           child: ListView(
         children: <Widget>[
           buildRaisedButton('registeration', 'Registration'),
-          buildRaisedButton('b', 'Breakfast-1'),
-          buildRaisedButton('c', 'Lunch-1'),
-          buildRaisedButton('d', 'Hi-Tea-1'),
-          buildRaisedButton('e', 'Gala dinner'),
-          buildRaisedButton('f', 'Breakfast-2'),
-          buildRaisedButton('g', 'Lunch-2'),
-          buildRaisedButton('h', 'Hi-Tea-2'),
-          buildRaisedButton('i', 'Ball entry')
+          buildRaisedButton('breakfast-1', 'Breakfast-1'),
+          buildRaisedButton('lunch-1', 'Lunch-1'),
+          buildRaisedButton('hi-tea-1', 'Hi-Tea-1'),
+          buildRaisedButton('gala-dinner', 'Gala dinner'),
+          buildRaisedButton('breakfast-2', 'Breakfast-2'),
+          buildRaisedButton('lunch-2', 'Lunch-2'),
+          buildRaisedButton('hi-Tea-2', 'Hi-Tea-2'),
+          buildRaisedButton('ball-entry', 'Ball entry')
         ],
       )),
     );
